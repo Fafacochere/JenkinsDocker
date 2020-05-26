e0e24e9d55661d7e788ccbfca58cf3d69fcd7818
Fabien Raulline <f.raulline@aurafinance.fr>
2020-05-26 12:06:56 +0200

    Testouille

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Jenkins-Docker");fehfeifhe;
+            Console.WriteLine("Hello Jenkins-Docker");fehfeifhe
         }
     }
 }
